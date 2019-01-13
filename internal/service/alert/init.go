8a9116889682b6ae84b1d32dce3990ea0e2d2cbb
Ethan <445366464@qq.com>
2019-01-13 18:33:01 +0800

    finish primary alert

--- internal/service/alert/init.go
+++ internal/service/alert/init.go
@@ -1,10 +1,18 @@
 package alert
 
+import (
+	"fmt"
+
+	"github.com/linthan/gocron/internal/service/alert/model"
+)
+
 //Service 服务
 type Service struct {
 }
 
 //Init 初始化
 func Init() *Service {
+	c := model.New()
+	fmt.Println("----", c)
 	return &Service{}
 }
--- internal/service/alert/model/alert.go
+++ internal/service/alert/model/alert.go
@@ -1,3 +1,78 @@
 package model
 
-type Alert struct{}
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+//Alert 报警实例
+type Alert struct {
+	Status       string
+	alertTimer   *time.Timer
+	level        int
+	done         chan int
+	assign       chan int
+	assignTicker *time.Ticker
+	assignee     string
+}
+
+//New 初始化一个报警
+func New() *Alert {
+	a := &Alert{
+		alertTimer: time.NewTimer(20 * time.Second),
+		done:       make(chan int),
+		assign:     make(chan int),
+		level:      0,
+		Status:     "active",
+	}
+	go a.Escalation()
+	return a
+}
+
+//Escalation 初始化
+func (a *Alert) Escalation() {
+	c := context.Background()
+	c.Done()
+	for {
+		select {
+		case <-a.alertTimer.C:
+			a.alertTimer.Reset(10 * time.Second)
+			a.level++
+			fmt.Println("----", a.level)
+			if a.level == 3 {
+				a.Done()
+			}
+		case <-a.assign:
+			a.Status = "assign"
+			return
+		case <-a.done:
+			fmt.Println("-------done")
+			a.Status = "done"
+			return
+		}
+	}
+}
+
+//AssignTo 指派给谁
+func (a *Alert) AssignTo(assignee string) {
+	a.assignTicker = time.NewTicker(5 * time.Second)
+	//指派给谁
+	a.assignee = assignee
+	a.assign <- 0
+	//发送报警
+
+	for {
+		select {
+		case <-a.assignTicker.C:
+		case <-a.done:
+			a.Status = "done"
+			return
+		}
+	}
+}
+
+//Done 关闭
+func (a *Alert) Done() {
+	close(a.done)
+}
